a4cecd9dc203fc04986cf0608a44cbc327649e97
Yan Yee Kwok <beverly@Yans-MacBook-Pro.local>
2020-10-18 17:42:21 -0700

    Create Index, Register and Login page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using JobTracker.Models;
 
@@ -20,6 +23,42 @@ namespace JobTracker.Controllers
 
         [HttpGet("")]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost("creat_user")]
+        public IActionResult CreateUser(User newUser)
+        {
+            if(ModelState.IsValid)
+            {
+                if(dbContext.Users.Any(u => u.Email == newUser.Email))
+                {
+                    ModelState.AddModelError("Email", "Email already in use!");
+                    return RedirectToAction("Register");
+                }
+                PasswordHasher<User> Hasher = new PasswordHasher<User>();
+                newUser.Password = Hasher.HashPassword(newUser, newUser.Password);
+                dbContext.Add(newUser);
+                dbContext.SaveChanges();
+                HttpContext.Session.SetInt32("UserId", newUser.UserId);
+                int? theUserId = HttpContext.Session.GetInt32("UserId");
+                return RedirectToAction("Home",new{UserId = theUserId});
+            }
+            else
+            {
+                return View(); //the dashboard name
+            }
+        }
+
+        [HttpGet("Signin")]
+        public IActionResult Signin()
         {
             return View();
         }
